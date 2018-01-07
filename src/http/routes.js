068687664b96c6117b48bba42dd06a2fc921fcbd
paprikaLang <3060866223@qq.com>
2018-01-07 18:25:21 +0800

    设置post请求添加restify.plugins.bodyParser()

--- src/http/routes.js
+++ src/http/routes.js
@@ -5,6 +5,17 @@ const routes = (server) => {
 		res.send('Enjoy yourself!')
 		next()
 	})
+	server.get('category',(req,res,next) => {
+        res.send(['1','paprika'])
+		next()
+	})
+	
+	server.post('category',(req,res,next) => {
+        const {name} = req.params
+        res.send(name)
+		next()
+	})
+
 }
 
 
