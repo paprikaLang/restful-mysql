772fa5c198afb573b385db59ff13d954220be296
paprikaLang <3060866223@qq.com>
2018-01-26 08:42:59 +0800

    完善错误信息整合

--- src/services/mysql/categories.js
+++ src/services/mysql/categories.js
@@ -5,11 +5,13 @@ const categories = deps => {
   return {
   	all: () => {
   	   return new Promise((resolve,reject) => {
-			const { connection } = deps
+			const { connection, errorHandler } = deps
 			connection.query('SELECT * FROM categories',(error,results) => {
 
 				if (error) {
-					reject(error)
+					// reject(error)
+					errorHandler(error, 'failed connect to db', reject)
+					return false
 				}
 				resolve({ pagination: {page: 1, results: results.length}, categories:results })
 
