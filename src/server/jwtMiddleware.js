b6de4bb8bdd75a85367bae95eb8c06d11273511b
paprikaLang <3060866223@qq.com>
2018-04-26 16:13:44 +0800

    完成jwt

--- src/server/jwtMiddleware.js
+++ src/server/jwtMiddleware.js
@@ -11,15 +11,22 @@ const jwtMiddleware = (deps) => {
 				res.send(403, {error: 'Token not exist'})
 				return false
 			}
-			await jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
-				if (error) {
-					res.send(403,{error: 'failed authenticate token'})
-				} else {
-					console.log (decoded)
-					//没有排除在外的路由可以获取解析信息
-					req.decoded = decoded
-				}
-			})
+			try {
+			   req.decoded = jwt.verify(token,process.env.JWT_SECRET)
+			} catch (error) {
+				res.send(403,{error: 'failed authenticate token'})
+				return false
+			}
+			// await jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+			// 	if (error) {
+			// 		//控制台过多的node错误讯息
+			// 		res.send(403,{error: 'failed authenticate token'})
+			// 	} else {
+			// 		console.log (decoded)
+			// 		//没有排除在外的路由可以获取解析信息
+			// 		req.decoded = decoded
+			// 	}
+			// })
 
         }
 	
